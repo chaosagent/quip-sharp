1e65b9f9aa186b3962fa7f938f6b6e74b6df2d3d
David Hou <david.hou314@gmail.com>
2023-11-30 15:12:20 -0800

    move e8p out

--- quiptools/setup.py
+++ quiptools/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='quiptools_cuda',
-      ext_modules=[cpp_extension.CUDAExtension('quiptools_cuda', ['quiptools_wrapper.cpp', 'quiptools.cu'],extra_compile_args={'cxx': ['-g', '-lineinfo'],
+      ext_modules=[cpp_extension.CUDAExtension('quiptools_cuda', ['quiptools_wrapper.cpp', 'quiptools.cu', 'quiptools_e8p_gemv.cu'],extra_compile_args={'cxx': ['-g', '-lineinfo'],
                             'nvcc': ['-O2', '-arch=sm_86', '-g', '-Xcompiler', '-rdynamic', '-lineinfo']})],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
 
